5704c006f6301f129c343f29bedf6c6dcd17a690
PageVPetty <pagevpetty@live.com>
2017-06-06 15:42:10 -0600

    Update Tic-Tac-Toe.js

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -6,6 +6,40 @@ var updatePlayer = function (){
   playerSpan.innerHTML = player;
 };
 
+var checkTitles = function (tiles, symbol) {
+var match = true;
+  tiles.forEach(function (tile) {
+    if (tile.innerHTML != symbol) {
+      match = false;
+    }
+  });
+  return match;
+};
+
+var checkWinner = function () {
+  var symbol;
+  if (player == 1) {
+    symbol = "X";
+  }else {
+    symbol = "O";
+  }
+  var row1 = checkTiles(document.querySelectorAll(".row1"), symbol;
+  var row2 = checkTiles(document.querySelectorAll(".row2"), symbol;
+  var row3 = checkTiles(document.querySelectorAll(".row3"), symbol;
+  var col1 = checkTiles(document.querySelectorAll(".col2"), symbol;  
+  var col2 = checkTiles(document.querySelectorAll(".col2"), symbol;
+  var col3 = checkTiles(document.querySelectorAll(".col3"), symbol;
+  var diag1 = checkTiles(document.querySelectorAll(".diag1), symbol;
+  var diag2 = checkTiles(document.querySelectorAll(".diag2), symbol;                                                  
+                        
+                                                  
+        
+  if (row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2) {
+    alert("Player " + symbol + "won!");
+}
+};
+                       
+                        
 tiles.forEach(function (tile) {
   tile.onclick = function () {
     if (tile.classList.contains("played")) {
