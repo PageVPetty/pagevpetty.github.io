6390ec3a6125795318a323546d80d02cb00b4ad2
PageVPetty <pagevpetty@live.com>
2017-06-06 16:35:48 -0600

    Update 2048.js

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,7 +1,16 @@
 
-var newDiv = document.creatElement("div");
-
 var boardDiv = document.querySelector("#board");
 
-boardDiv.appendChild(newDiv);
+
+for (var i = 0; i <4; i++) {
+  var rowDiv = document.createElement("div");
+  rowDive.classList.add("row");
+  boardDiv.appendChild(rowDiv);
+  
+
+for (var j = 0; j <4; j++) {
+  var rowDiv = document.createElement("div");
+  tileDive.classList.add("tile");
+  rowDiv.appendChild(tileDiv);  
+  
 
