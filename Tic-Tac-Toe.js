fff757b79fdce818d34ad5993157c9b74de3bed7
PageVPetty <pagevpetty@live.com>
2017-06-06 14:52:30 -0600

    Update Tic-Tac-Toe.js

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,9 +1,15 @@
 var player = 1;
 var tiles = document.querySelectorAll("#board > div");
 
+var updatePlayer = function (){
+  var playerSpan.innerHTML = player;
+
 tiles.forEach(function (tile) {
   tile.onclick = function () {
     console.log(tile);
+    tile.classLIst.add("played")) {
+      return;
+    }
     tile.style.backgroundColor = "periwinkle";
     tile.classList.add("myClass");
     if (player == 1) {
