41bf14d14eafa9929b1a4f8b8041f931a189b43b
PageVPetty <pagevpetty@live.com>
2017-06-06 15:10:18 -0600

    Update Tic-Tac-Toe.js

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -2,20 +2,28 @@ var player = 1;
 var tiles = document.querySelectorAll("#board > div");
 
 var updatePlayer = function (){
-  var playerSpan.innerHTML = player;
+  var playerSpan = document.querySelector("#player");
+  playerSpan.innerHTML = player;
+};
 
 tiles.forEach(function (tile) {
   tile.onclick = function () {
-    console.log(tile);
-    tile.classLIst.add("played")) {
+    if (tile.classList.contains("played")) {
       return;
     }
-    tile.style.backgroundColor = "periwinkle";
-    tile.classList.add("myClass");
-    if (player == 1) {
-      ....
-    tile.innerHTML = "X";
+    tile.classList.add("played");
+     if (player == 1) {
+       tile.innerHTML = "X"
+       tile.style.color = "#0c0";
+       player = 2;
+    }  else {
+       tile.innerHTML = "0";
+       tile.style.color = "#00c";
+      player = 1;
+    }
+    updatePlayer();
   };
   
 });
+  updatePlayer();
 
