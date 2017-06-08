d83a3aa470ff217151c9f975ce14c50bb14432c2
12 <cs12@CIT-SCC112-1106.cs.dixie.edu>
2017-06-08 15:38:47 -0600

    kfjal

---
+++ 2048-stash/2048.js
@@ -0,0 +1,50 @@
+var board = {};
+
+var tileKey = function (col, row) {
+  return "tile" + col +
+}
+
+var createBoard = function () {
+  var boardDiv = document.querySelector("#board");
+
+  for (var row = 0; row < 4; row++) {
+    var rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    boardDiv.appendChild(rowDiv);
+
+    for (var col = 0; col <4; col++) {
+      var tileDiv = document.createElement("div");
+      var key = tileKey(col, row);
+      tileDiv.id = = key;
+      tileDive.classList.add("tile");
+      rowDiv.appendChild(tileDiv);
+    }
+  }
+};
+
+var updateBoard = function () {
+  for (var row = 0; row < 4; row++) {
+    for (var col = 0; col < 4; col++) {
+      var key = tileKey(col, row);
+      var value = board[key];
+      if (value) {
+        tileDiv.innerHTML = value;
+
+      } else {
+
+      }
+    }
+  }
+};
+
+var addTile = function () {
+  board["tile1-1"] = 4;
+};
+
+createBoard();
+
+addTile();
+console.log(board);
+document.onKeydown = function (){
+  
+}
